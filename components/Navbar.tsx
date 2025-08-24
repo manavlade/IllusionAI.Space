e903f86512d624ec323ad801e946f38103c9a434
manavlade <manavlade14690@gmail.com>
2025-08-25 01:03:05 +0530

    project initialized

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,7 @@ import {
 import { useState } from "react";
 import ContactPage from "./ContactUs";
 import Footer from "./Footer";
+import { WavyBackground } from "./ui/wavy-background";
 
 export function NavPage() {
     const navItems = [
@@ -99,7 +100,7 @@ export function NavPage() {
                     </MobileNavMenu>
                 </MobileNav>
             </Navbar>
-
+            <BackgroudBeam />
             <ContactPage />
             <Footer />
 
@@ -108,99 +109,24 @@ export function NavPage() {
     );
 }
 
-const DummyContent = () => {
+const BackgroudBeam = () => {
     return (
-        <div className="container mx-auto p-8 pt-24">
-            <h1 className="mb-4 text-center text-3xl font-bold">
-                Check the navbar at the top of the container
-            </h1>
-            <p className="mb-10 text-center text-sm text-zinc-500">
-                For demo purpose we have kept the position as{" "}
-                <span className="font-medium">Sticky</span>. Keep in mind that this
-                component is <span className="font-medium">fixed</span> and will not
-                move when scrolling.
-            </p>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
-                {[
-                    {
-                        id: 1,
-                        title: "The",
-                        width: "md:col-span-1",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 2,
-                        title: "First",
-                        width: "md:col-span-2",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 3,
-                        title: "Rule",
-                        width: "md:col-span-1",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 4,
-                        title: "Of",
-                        width: "md:col-span-3",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 5,
-                        title: "F",
-                        width: "md:col-span-1",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 6,
-                        title: "Club",
-                        width: "md:col-span-2",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 7,
-                        title: "Is",
-                        width: "md:col-span-2",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 8,
-                        title: "You",
-                        width: "md:col-span-1",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 9,
-                        title: "Do NOT TALK about",
-                        width: "md:col-span-2",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                    {
-                        id: 10,
-                        title: "F Club",
-                        width: "md:col-span-1",
-                        height: "h-60",
-                        bg: "bg-neutral-100 dark:bg-neutral-800",
-                    },
-                ].map((box) => (
-                    <div
-                        key={box.id}
-                        className={`${box.width} ${box.height} ${box.bg} flex items-center justify-center rounded-lg p-4 shadow-sm`}
-                    >
-                        <h2 className="text-xl font-medium">{box.title}</h2>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-};
+        <>
+            <WavyBackground>
+                <div className="max-w-6xl mx-auto p-4">
+                    <h1 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+                        <span>
+                            Illusion AI
+                        </span>  <br />Where AI Meets Infinite Wisdom
+
+                    </h1>
+                    <p></p>
+                    <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+                        AI that can read your future, solve your business problems, and teach you the secrets of the universe and help you to research — all in one place.
+                    </p>
+                </div>
+            </WavyBackground>
+
+        </>
+    )
+}
