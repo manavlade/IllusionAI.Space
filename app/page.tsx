3663c079241d16f785d4890589481139c182206c
manavlade <manavlade14690@gmail.com>
2025-08-25 00:51:05 +0530

    project initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ContactPage from "@/components/ContactUs";
 import Footer from "@/components/Footer";
+import { NavPage } from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <div>
+      <NavPage/>
       <ContactPage/>
       <Footer/>
     </div>
